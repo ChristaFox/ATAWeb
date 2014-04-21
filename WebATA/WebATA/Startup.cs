ad178c232971d77da7b1d91c7bcd8cd0a6472f9c
ChristaFox <christa.r.fox@me.com>
2014-04-21 11:57:17 -0600

    create with templates

---
+++ WebATA/WebATA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebATA.Startup))]
+namespace WebATA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
